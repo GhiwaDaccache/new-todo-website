019a302e98205dc0c64599540149009a464c4d6b
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-03-14 22:13:08 +0200

    added-signin

--- APIs/signin.php
+++ APIs/signin.php
@@ -1,3 +1,20 @@
 <?php
-
 include('connection.php');
+
+$user_input = $_POST['user_input'];
+$password = $_POST['password'];
+
+$check_count = $mysqli->prepare('select count(*) from users where email=? or username=?;');
+$check_count->bind_param('ss', $user_input, $user_input);
+$check_count->execute();
+$check_count->store_result();
+
+$check_count->bind_result($count);
+$check_count->fetch();
+
+
+if($count == 0){
+    echo 'no user found';
+}else{
+    echo 'succes';
+};
