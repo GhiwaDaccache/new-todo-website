40d78e8470b133c8f34533fc377c8ea6568eece8
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-03-15 02:14:13 +0200

    added-read-js

--- UI/scripts/read.js
+++ UI/scripts/read.js
@@ -1,12 +1,40 @@
 const urlParams = new URLSearchParams(window.location.search);
 const paramValue = urlParams.get('user_id');
+const personalTodo = document.getElementById('personal');
+const  professionalTodo = document.getElementById('professional');
+const choresTodo = document.getElementById('chores');
+
+
 
 axios.get(`http://localhost/new-todo-website/APIs/read.php?user_id=${paramValue}`)
   .then(response => {
     
-    console.log('Response:', response.data);
-    
+    const todoList = response.data.todos;
+    const personal = todoList.filter(item => item["category_id"] == 1);
+    const professional = todoList.filter(item => item["category_id"] == 2);
+    const chores = todoList.filter(item => item["category_id"] == 3);
+    personalTodo.innerHTML = "";
+    professionalTodo.innerHTML = "";
+    choresTodo.innerHTML = "";
+
+    personal.forEach(element => {
+        personalTodo.innerHTML += `<div class="card-header primary-bg"></div>
+                                    <div class="flex align-center full-w">
+                                    <i class="fa-regular fa-square-check"></i>
+                                    <p>${element.description}</p>
+                                </div>`
+    });
+
+    professional.forEach(element => {
+        todoCard.innerHTML += `<div class="card-header primary-bg"></div>
+                                    <div class="flex align-center full-w">
+                                    <i class="fa-regular fa-square-check"></i>
+                                    <p>${element.description}</p>
+                                </div>`
+    });
+
   })
   .catch(error => {
     console.error('Error:', error);
   });
+
