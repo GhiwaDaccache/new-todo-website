8ee4800f0884137d88e48e6b05c64c832b3aff6f
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-03-15 02:22:59 +0200

    added-create-api

---
+++ APIs/create.php
@@ -0,0 +1,15 @@
+<?php
+include('connection.php');
+
+$description = $_POST['description'];
+$category_id = $_POST['category_id'];
+$user_id = $_POST['user_id'];
+
+$create_todo = $mysqli->prepare('insert into todos (description, category_id, user_id) values(?,?,?) ');
+$create_todo->bind_param('sii', $description, $category_id, $user_id);
+$create_todo->execute();
+$create_todo->store_result();
+
+$response['status'] = "success";
+
+echo json_encode($response);
